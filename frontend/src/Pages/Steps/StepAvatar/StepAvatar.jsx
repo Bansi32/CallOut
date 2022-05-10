5feceb61e70502545f01c0e6b2ec48edeb0382f3
Bansi <jpatel19762001@gmail.com>
2022-05-10 19:31:43 +0530

    profile name half done 5th v

--- frontend/src/Pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/Pages/Steps/StepAvatar/StepAvatar.jsx
@@ -5,19 +5,19 @@ import style from './StepAvatar.module.css';
 import { useSelector } from 'react-redux';
 import styles from '../../../Pages/Home/Home.module.css';
 
-export default function StepAvatar({onNext}) {
-  const { name } = useSelector((state)=>state.activate);
-  function submit() {}
+export default function StepAvatar({ onNext }) {
+  const { name } = useSelector((state) => state.activate);
+  function submit() { }
   return (
     <>
-    <div className={styles.cardWrapper}>
-      <Card title={`Hello, ${name}!`} icon="monkey">
-        <p className={style.subheading}>Your Profile Photo</p>
-        <div>
-          <Button onClick={submit}>Next</Button>
-        </div>
-      </Card>
-    </div>
-    </>    
+      <div className={styles.cardWrapper}>
+        <Card title={`Hello, ${name}!`} icon="monkey">
+          <p className={style.subheading}>Your Profile Photo</p>
+          <div>
+            <Button onClick={submit} type="Submit"></Button>
+          </div>
+        </Card>
+      </div>
+    </>
   );
 };
